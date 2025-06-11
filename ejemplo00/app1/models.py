9ca36386e594f4d56e6d110e2279d5c591293981
SantiagoVSR <santiago9sr@hotmail.com>
2025-06-11 09:01:28 -0500

    actualizacion02

--- ejemplo00/app1/models.py
+++ ejemplo00/app1/models.py
@@ -1,3 +1,22 @@
 from django.db import models
 
 # Create your models here.
+
+class Estudiante(models.Model):
+	nombre = models.CharField(max_length = 30)
+	apellido = models.CharField(max_length = 30)
+	cedula = models.CharField(max_length = 30 , unique=True)
+	edad = models.IntegerField()
+
+	def __str__(self):
+		return "%$ - %$ - %$ - edad: %d" % (self.nombre,
+			self.apellido,
+			self.cedula,
+			self.edad)
+
+class Ciudad(models.Model):
+	Nombre = models.CharField(max_length = 30)
+	Poblacion = models.IntegerField()
+
+	def __str__(self):
+		return f"{self.nombre} - {self.poblacion}"
